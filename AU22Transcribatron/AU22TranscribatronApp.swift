e13b1287198d36c5dcafecc6d5f523eb59949cce
ScreenBottle <arvid.rydh@iths.se>
2023-01-24 12:56:59 +0100

    Initial Commit

---
+++ AU22Transcribatron/AU22TranscribatronApp.swift
@@ -0,0 +1,17 @@
+//
+//  AU22TranscribatronApp.swift
+//  AU22Transcribatron
+//
+//  Created by Arvid Rydh on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct AU22TranscribatronApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
